125e0985df9415ba97685cdcde9fd2da60c39fd3
fengyang.zhang <fengyang.zhang@busonline.com>
2017-03-20 11:25:16 +0800

    add slf4j

--- src/main/java/org/mybatis/dao/TestDao.java
+++ src/main/java/org/mybatis/dao/TestDao.java
@@ -1,6 +1,8 @@
 package org.mybatis.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.mybatis.entity.TestEntity;
 
 public interface TestDao {
@@ -9,5 +11,9 @@ public interface TestDao {
 
 	@Select("SELECT * FROM TEST WHERE ID=#{id}")
 	public TestEntity selectOne2(int id);
+	
+	@Insert("insert into test (id, name) values(#{id}, #{name})")
+	@SelectKey(statement="call next value for TestSequence", keyProperty="id", before=true, resultType=int.class)
+	int insertEntity(TestEntity test);
 
 }
